0f027b12426c7bf78ed3a6998f74a7cd6f1b0025
Sajay Antony <sajaya@microsoft.com>
2022-05-12 23:06:06 +0000

    OCI wg-references-types support

--- go.mod
+++ go.mod
@@ -1,20 +1,17 @@
-module github.com/sajayantony/oras-manifest
+module github.com/sajayantony/reftype
 
 go 1.17
 
 require (
-	oras.land/oras-go/pkg v0.0.0
-	oras.land/oras-go/v2 v2.0.0
+	github.com/opencontainers/image-spec v1.0.2
+	oras.land/oras-go/v2 v2.0.0-00010101000000-000000000000
 )
 
 require (
+	github.com/oci-playground/artifact-spec v0.0.0-20220506233500-8fed0a29d06f // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
-	github.com/opencontainers/image-spec v1.0.2 // indirect
 	github.com/oras-project/artifacts-spec v1.0.0-draft.1.1 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 )
 
-replace (
-	oras.land/oras-go/pkg => /home/sajay/code/src/github.com/sajayantony/oras-go/pkg
-	oras.land/oras-go/v2 => /home/sajay/code/src/github.com/sajayantony/oras-go
-)
+replace oras.land/oras-go/v2 => github.com/sajayantony/oras-go/v2 v2.0.0-20220512230320-01aec7d64b23
--- main.go
+++ main.go
@@ -2,42 +2,77 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
 	"os"
 	"strings"
 
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	registry "oras.land/oras-go/v2/registry/remote"
 )
 
 func main() {
 
-	flag.Parse()
+	mnftcmd := flag.NewFlagSet("manifest", flag.ExitOnError)
+	refscmd := flag.NewFlagSet("refs", flag.ExitOnError)
 
-	if flag.NArg() == 0 {
-		fmt.Println("No reference specified")
+	if len(os.Args) < 2 {
+		// switch to better CLI parsing
+		fmt.Print("No subcommand specified")
 		os.Exit(1)
 	}
+	cmd := os.Args[1]
+	switch cmd {
+	case "manifest":
+		err := mnftcmd.Parse(os.Args[2:])
+		if err != nil {
+			panic(err)
+		}
+		if mnftcmd.NArg() == 0 {
+			fmt.Println("No reference specified")
+			os.Exit(1)
+		}
 
-	ref := flag.Arg(0)
-	mnft, err := fetchManifest(ref)
-	if err != nil {
-		fmt.Print(err)
+		ref := mnftcmd.Arg(0)
+		mnft, err := fetchManifest(ref)
+		if err != nil {
+			fmt.Print(err)
+		}
+		fmt.Println(mnft)
+
+	case "ls":
+		err := refscmd.Parse(os.Args[2:])
+		if err != nil {
+			panic(err)
+		}
+		if refscmd.NArg() == 0 {
+			fmt.Println("No reference specified")
+			os.Exit(1)
+		}
+
+		ref := refscmd.Arg(0)
+		if err := fetchReferrers(ref); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	default:
+		fmt.Printf("Invalid subcommand %s\n", cmd)
+		os.Exit(1)
 	}
-	fmt.Println(mnft)
 }
 
 func fetchManifest(ref string) (string, error) {
 	//	ref = "localhost:5000/hello-world:latest@sha256:f54a58bc1aac5ea1a25d796ae155dc228b3f0e11d046ae276b39c4bf2f13d8c4"
-	fmt.Println(ref)
+	// fmt.Println(ref)
 	ctx := context.Background()
 	repo, err := registry.NewRepository(ref)
 	if err != nil {
 		panic(err)
 	}
+	setPlainHttp(repo)
 
-	repo.PlainHTTP = true
 	_, rc, err := repo.FetchReference(ctx, ref)
 	if err != nil {
 		panic(err)
@@ -49,4 +84,40 @@ func fetchManifest(ref string) (string, error) {
 		return "", err
 	}
 	return buf.String(), err
+}
+
+func fetchReferrers(ref string) error {
+
+	ctx := context.Background()
+	repo, err := registry.NewRepository(ref)
+
+	if err != nil {
+		panic(err)
+	}
+
+	setPlainHttp(repo)
+
+	desc, err := repo.Manifests().Resolve(ctx, ref)
+	if err != nil {
+		return err
+	}
+	if err = repo.Referrers(ctx, desc, func(refs []ocispec.Descriptor) error {
+		for _, r := range refs {
+			d, err := json.Marshal(r)
+			if err == nil {
+				fmt.Println(string(d))
+			}
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setPlainHttp(repo *registry.Repository) {
+	if repo.Reference.Host() == "localhost" || strings.HasPrefix(repo.Reference.Host(), "localhost:") {
+		repo.PlainHTTP = true
+	}
 }
